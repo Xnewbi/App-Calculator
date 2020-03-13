676f4af6dbaba187e0dd31c3d43dd47ded34eb5e
Xnewbi <dian.pratama@students.amikom.ac.id>
2020-03-14 01:37:41 +0700

    final calculator

--- Calculator.cs
+++ Calculator.cs
@@ -1,8 +1,11 @@
 using System;
 
-namespace Calculator{
-	public class Calculator{
-		public void Main(string[] args){
+namespace Calculator
+{
+	class Calculator
+	{
+		static void Main(string[] args)
+		{
 			int a;
 			int b;
 			int pil;
@@ -13,40 +16,49 @@ namespace Calculator{
 			Console.WriteLine("3. Perkalian \n");
 			Console.WriteLine("4. Pembagian \n");
 			Console.WriteLine("Masukan pilihan menu : ");
-			pil = Convert.ToInt32(Console.ReadLine()); 
-			
+			pil = Convert.ToInt32(Console.ReadLine());
+
 			Console.WriteLine("Masukan nilai A : ");
 			a = Convert.ToInt32(Console.ReadLine());
-			
+
 			Console.WriteLine("Masukan nilai B : ");
 			b = Convert.ToInt32(Console.ReadLine());
 
-			if(pil == 1){
-				Console.WriteLine("Hasil penambahan: {0} + {1} = {2}", a,b, Penambahan(a,b));
+			if (pil == 1)
+			{
+				Console.WriteLine("Hasil penambahan: {0} + {1} = {2}", a, b, Penambahan(a, b));
 			}
-			if(pil == 2){
-				Console.WriteLine("Hasil penambahan: {0} + {1} = {2}", a,b, Pengurangan(a,b));
+			if (pil == 2)
+			{
+				Console.WriteLine("Hasil penambahan: {0} + {1} = {2}", a, b, Pengurangan(a, b));
 			}
-			if(pil == 3){
-				Console.WriteLine("Hasil penambahan: {0} + {1} = {2}", a,b, Perkalian(a,b));
+			if (pil == 3)
+			{
+				Console.WriteLine("Hasil penambahan: {0} + {1} = {2}", a, b, Perkalian(a, b));
 			}
-			if(pil == 4){
-				Console.WriteLine("Hasil penambahan: {0} + {1} = {2}", a,b, Pembagian(a,b));
+			if (pil == 4)
+			{
+				Console.WriteLine("Hasil penambahan: {0} + {1} = {2}", a, b, Pembagian(a, b));
 			}
 		}
-		
-		static int Penambahan(int a, int b){
+
+		static int Penambahan(int a, int b)
+		{
 			return a + b;
 		}
-		static int Pengurangan(int a, int b){
+		static int Pengurangan(int a, int b)
+		{
 			return a - b;
 		}
-		static int Perkalian(int a, int b){
+		static int Perkalian(int a, int b)
+		{
 			return a - b;
 		}
-		static int Pembagian(int a, int b){
+		static int Pembagian(int a, int b)
+		{
 			return a - b;
 		}
 
 	}
-	
+
+}
